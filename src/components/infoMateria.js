68483bb01ff47f98ca7996e5ae2c7bc2788f1027
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-03-17 17:55:43 -0300

    falta CSS y q las canciones se reproduzcan con los botones de abajo

---
+++ src/components/infoMateria.js
@@ -0,0 +1,15 @@
+// GET: /sounds/all
+
+//     {
+//         data/fx.json: [],
+//         data/songs.json: []
+//     }
+
+
+//     GET: /sounds/songs
+
+//     [
+//         { "id":1, "category":"game", "name":"Mario Castle", "url":"files/mario/songs/castle.mp3" },
+//         { "id":2, "category":"game", "name":"Mario Star", "url":"files/mario/songs/hurry-starman.mp3"},
+//         { "id":3, "category":"game", "name":"Mario Overworld", "url":"files/mario/songs/overworld.mp3"}
+//     ]

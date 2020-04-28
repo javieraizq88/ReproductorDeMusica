a9601b6899861254894e97cf3f925a39c7bfc310
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-04-28 15:16:33 -0400

    cambio background y vistas

--- src/components/ListaDeMusica.js
+++ src/components/ListaDeMusica.js
@@ -15,21 +15,22 @@ const ListaAudio = props => {
                             // hay q concatenar 2 url para q sea 1 cancion
                             return (
                                 <li key={i} >{elem.name}
-                                <audio controls>
-                                    <source src={"https://assets.breatheco.de/apis/sound/"+ elem.url} type="audio/ogg"/>
-                                    <source src="music.mp3" type="audio/mpeg"/>
-                                </audio>
+                                    <audio controls>
+                                        <source src={"https://assets.breatheco.de/apis/sound/" + elem.url} type="audio/ogg" />
+                                        <source src="music.mp3" type="audio/mpeg" />
+                                    </audio>
                                 </li>
                             )
                         })
-                    }   
+                    }
+
                     <div className="card-body">
                         <button onClick="backward()" type="button" className="col-1 fas fa-backward"></button>
                         <button onClick="play()" type="button" className="col-1 fas fa-play"></button>
                         <button onClick="forward()" type="button" className="col-1 fas fa-forward"></button>
                     </div>
                 </div>
-                <div className=" col-3"></div>
+                <div className=" col-md-3"></div>
             </div>
         </>
     )
